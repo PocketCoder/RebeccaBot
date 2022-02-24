adf94f19c1937a3bdcc003a15d1f0bd8d506b19f
PocketCoder <jake.williams77@gmail.com>
2022-02-24 10:30:29 +0000

    Init commit 2

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const prefix = "!";
+
+client.on("messageCreate", message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        message.reply('Pong!');
+    }
+});
+
+client.login(config.BOT_TOKEN);
