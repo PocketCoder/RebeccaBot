cd32ff66871601fd3c338eff509469f3385cc0af
PocketCoder <jake.williams77@gmail.com>
2022-03-02 11:36:30 +0000

    New schema for tracking all books chosen as book of the month

---
+++ models/history.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const historySchema = new mongoose.Schema({
+    username: mongoose.SchemaTypes.String,
+    book: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    author: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    month: {
+        type: mongoose.SchemaTypes.Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('History', historySchema);
