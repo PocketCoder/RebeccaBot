e059d458cb0f6cf0d4196b3e7e044e10b9d33f7d
PocketCoder <jake.williams77@gmail.com>
2022-03-03 15:04:45 +0000

    feat: Regex testing for suggestion commands

--- commands/suggestionComm.js
+++ commands/suggestionComm.js
@@ -13,30 +13,35 @@ module.exports = {
             }).exec();
             message.reply(`Your current suggestion is ${userSuggest.book} by ${userSuggest.author}.`);
         } else {
-            const title = args.slice(0, args.indexOf('by')).join(" ");
-            const author = args.slice(args.indexOf('by') + 1, args.length).join(" ");
-            const hist = await History.findOne({
-                book: title
-            });
-            if (hist === null) {
-                Suggestion.findOneAndUpdate({
-                    userId: message.author.id
-                }, {
-                    userId: message.author.id,
-                    username: message.author.username,
-                    book: title,
-                    author: author
-                }, {
-                    upsert: true
-                }, (e) => {
-                    if (e === null) {
-                        message.reply('What a wonderful choice! Let me just make a note of that.');
-                    } else {
-                        message.reply(e);
-                    }
+            const sugReg = new RegExp(/((\w\s)?(\w)+)+(\s\w[by]\s)((\w)+(\s\w)?)+/g);
+            if (sugReg.test(args.join(" "))) {
+                const title = args.slice(0, args.indexOf('by')).join(" ");
+                const author = args.slice(args.indexOf('by') + 1, args.length).join(" ");
+                const hist = await History.findOne({
+                    book: title
                 });
+                if (hist === null) {
+                    Suggestion.findOneAndUpdate({
+                        userId: message.author.id
+                    }, {
+                        userId: message.author.id,
+                        username: message.author.username,
+                        book: title,
+                        author: author
+                    }, {
+                        upsert: true
+                    }, (e) => {
+                        if (e === null) {
+                            message.reply('What a wonderful choice! Let me just make a note of that.');
+                        } else {
+                            message.reply(e);
+                        }
+                    });
+                } else {
+                    message.reply("Oops! Looks like we've already read that one. Use *!history* to see all the books we've read in the past.");
+                }
             } else {
-                message.reply("Oops! Looks like we've already read that one. Use *!history* to see all the books we've read in the past.");
+                message.reply('Sorry, that doesn\'t seem to be a complete request. Make sure you pass your suggestion as <book title> by <book author>');
             }
         }
     }
