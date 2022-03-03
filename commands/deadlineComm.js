58b2a6d7cd1e722edbda3f27afa664e1c62d02f2
PocketCoder <jake.williams77@gmail.com>
2022-03-03 13:54:44 +0000

    Permission added for role in testing server

--- commands/deadlineComm.js
+++ commands/deadlineComm.js
@@ -7,36 +7,39 @@ module.exports = {
     name: 'deadline',
     description: '',
     async execute(message, args) {
-        // TODO: Make a Mod only command.
-        if (args.length === 0) {
-            const deadline = await Deadline.find({});
-            if (deadline[0] != null) {
-                const dc = deadline[0].dateString.split('/');
-                // TODO: Fix days until.
-                const until = deadline[0].date.getTime() - new Date().getTime();
-                const days = Math.floor(until / (1000 * 3600 * 24));
-                message.reply(`The next meeting will be on **${dc[0]}/${dc[1]}**. That gives you **${days} days**.`);
+        if (message.member.role.cache.has('948939449243402241')) {
+            if (args.length === 0) {
+                const deadline = await Deadline.find({});
+                if (deadline[0] != null) {
+                    const dc = deadline[0].dateString.split('/');
+                    // TODO: Fix days until.
+                    const until = deadline[0].date.getTime() - new Date().getTime();
+                    const days = Math.floor(until / (1000 * 3600 * 24));
+                    message.reply(`The next meeting will be on **${dc[0]}/${dc[1]}**. That gives you **${days} days**.`);
+                } else {
+                    message.reply("Hmmm, there doesn't seem to be a deadline yet. Listen out for further announcements from the moderator. In the meantime, keep reading!");
+                }
             } else {
-                message.reply("Hmmm, there doesn't seem to be a deadline yet. Listen out for further announcements from the moderator. In the meantime, keep reading!");
+                const contents = await Deadline.find({});
+                contents.forEach(async (o) => {
+                    await Deadline.deleteOne({
+                        _id: o._id
+                    });
+                });
+                var dc = args[0].split('/');
+                dc.map(x => {
+                    x = x.valueOf();
+                });
+                dc[1] = dc[1] - 1; // Because January is 0!
+                const deadline = new Date(dc[2], dc[1], dc[0]);
+                message.reply(`Ok, listen up @everyone. You've got until **${dc[0]}/${dc[1]+1}** to read this month's book. Ready...Set...Go!`);
+                await new Deadline({
+                    date: deadline,
+                    dateString: args[0]
+                }).save();
             }
         } else {
-            const contents = await Deadline.find({});
-            contents.forEach(async (o) => {
-                await Deadline.deleteOne({
-                    _id: o._id
-                });
-            });
-            var dc = args[0].split('/');
-            dc.map(x => {
-                x = x.valueOf();
-            });
-            dc[1] = dc[1] - 1; // Because January is 0!
-            const deadline = new Date(dc[2], dc[1], dc[0]);
-            message.reply(`Ok, listen up @everyone. You've got until **${dc[0]}/${dc[1]+1}** to read this month's book. Ready...Set...Go!`);
-            await new Deadline({
-                date: deadline,
-                dateString: args[0]
-            }).save();
+            message.reply('Sorry, you don\'t have permission to do that.');
         }
     }
 }
--- commands/shuffleComm.js
+++ commands/shuffleComm.js
@@ -2,65 +2,72 @@ const Suggestion = require('../models/suggestionSchema.js');
 const History = require('../models/historySchema.js');
 const Counter = require('../models/counterSchema.js');
 const Deadline = require('../models/deadlineSchema.js');
-const { listenerCount } = require('../models/suggestionSchema.js');
+const {
+    listenerCount
+} = require('../models/suggestionSchema.js');
 
 module.exports = {
     name: 'shuffle',
     description: '',
     async execute(message, args) {
-        // TODO: Make a Mod only command
-        // TODO: Make it cleaner. There has to be a better way.
-        const list = await Suggestion.find({}).exec();
-        const counters = await Counter.find({}).exec();
-        var lowest = counters[0].count;
-        await counters.forEach(e => {
-            var curr = e.count;
-            if (curr < lowest) {
-                lowest = curr;
-            }
-        });
-        var lowArray = [];
-        await counters.forEach(e => {
-            if (e.count === lowest) {
-                lowArray.push(e);
-            }
-        });
-        var choices = [];
-        await list.forEach((e, i) => {
-            lowArray.forEach(l => {
-                if (e.userId === l.userId) {
-                    choices.push(e);
+        if (message.member.role.cache.has('948939449243402241')) {
+            // TODO: Make it cleaner. There has to be a better way.
+            const list = await Suggestion.find({}).exec();
+            const counters = await Counter.find({}).exec();
+            var lowest = counters[0].count;
+            await counters.forEach(e => {
+                var curr = e.count;
+                if (curr < lowest) {
+                    lowest = curr;
                 }
-            })
-        });
-        const choice = Math.floor(Math.random() * choices.length);
-        const date = `${new Date().getMonth().toString()}/${new Date().getFullYear().toString()}`;
-        const bom = new History({
-            userId: choices[choice].userId,
-            username: choices[choice].username,
-            book: choices[choice].book,
-            author: choices[choice].author,
-            date: date
-        });
-        await bom.save();
-        var newCounter;
-        const currCounter = await Counter.findOne({
-            userId: choices[choice].userId
-        }).exec();
-        if (currCounter != null) {
-            newCounter = currCounter.count + 1;
+            });
+            var lowArray = [];
+            await counters.forEach(e => {
+                if (e.count === lowest) {
+                    lowArray.push(e);
+                }
+            });
+            var choices = [];
+            await list.forEach((e, i) => {
+                lowArray.forEach(l => {
+                    if (e.userId === l.userId) {
+                        choices.push(e);
+                    }
+                })
+            });
+            const choice = Math.floor(Math.random() * choices.length);
+            const date = `${new Date().getMonth().toString()}/${new Date().getFullYear().toString()}`;
+            const bom = new History({
+                userId: choices[choice].userId,
+                username: choices[choice].username,
+                book: choices[choice].book,
+                author: choices[choice].author,
+                date: date
+            });
+            await bom.save();
+            var newCounter;
+            const currCounter = await Counter.findOne({
+                userId: choices[choice].userId
+            }).exec();
+            if (currCounter != null) {
+                newCounter = currCounter.count + 1;
+            } else {
+                newCounter = 1;
+            }
+            await Counter.findOneAndUpdate({
+                userId: choices[choice].userId
+            }, {
+                username: choices[choice].username,
+                count: newCounter
+            }, {
+                upsert: true
+            });
+            message.reply(`@everyone this month’s book is **${choices[choice].book} by ${choices[choice].book}**, which was @${choices[choice].username}’s suggestion!`);
+            await Suggestion.deleteOne({
+                userId: choices[choice].userId
+            });
         } else {
-            newCounter = 1;
+            message.reply('Sorry, you don\'t have permission to do that.');
         }
-        await Counter.findOneAndUpdate({
-            userId: choices[choice].userId
-        }, {
-            username: choices[choice].username,
-            count: newCounter
-        }, {
-            upsert: true
-        });
-        message.reply(`@everyone this month’s book is **${choices[choice].book} by ${choices[choice].book}**, which was @${choices[choice].username}’s suggestion!`);
-        await Suggestion.deleteOne({userId: choices[choice].userId});
     }
 }
